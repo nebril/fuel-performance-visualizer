567aa85871604aec673aa59ba33915a956630162
Maciej Kwiek <mkwiek@mirantis.com>
2015-02-09 16:33:19 +0100

    Downloading and extracting tarballs

---
+++ generator/dot2jsonparser.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+import json
+import sys
+
+import pydot
+
+
+class Dot2JSONParser(object):
+    def __init__(self, sourcefile):
+        self.source = sourcefile
+
+    def parse(self):
+        self.graph = pydot.graph_from_dot_file(self.source)
+
+        nodes = self._get_nodes()
+        edges = self._get_edges()
+
+        return json.dumps({
+            "nodes": nodes,
+            "edges": edges,
+        })
+
+    def _get_nodes(self):
+        nodes = []
+        for node in self.graph.get_node_list():
+            try:
+                function, percentage, percentage2, times = tuple(
+                    node.get_label().split('\\n'))
+                nodes.append({
+                    "data": {
+                        "id": str(int(node.get_name())),
+                        "funcName": function,
+                        "percentage": float(percentage.strip('()%')),
+                        "percentage2": float(percentage2.strip('()%')),
+                        "callCount": times,
+                        "label": "{0}, {1}, {2}".format(
+                            function, percentage, times),
+                    },
+                    "position": {
+                        "x": 0,
+                        "y": 0,
+                    },
+                })
+
+            except (ValueError, AttributeError):
+                sys.stderr.write("Skipped {0}".format(node.get_name()))
+                pass
+        return nodes
+
+    def _get_edges(self):
+        edges = []
+
+        for edge in self.graph.get_edge_list():
+            percentage, times = tuple(
+                edge.get_label().strip('"').split('\xc3')[0].split('\\n')
+            )
+            edges.append({
+                "data": {
+                    "id": 'e' + (str(int(edge.get_source()))
+                                 + '_'
+                                 + str(int(edge.get_destination()))),
+                    "source": edge.get_source(),
+                    "target": edge.get_destination(),
+                    "percentage": float(percentage.strip('()%')),
+                    "callCount": int(times),
+                    "label": "{0}, {1}x".format(percentage, times),
+                }
+            })
+
+        return edges
--- generator/generate_data.py
+++ generator/generate_data.py
@@ -2,16 +2,23 @@
 
 import json
 import urllib
+import subprocess
 
 
-BUILD_URL = ('https://fuel-jenkins.mirantis.com/job/'
+LAST_BUILD_URL = ('https://fuel-jenkins.mirantis.com/job/'
              'nailgun_performance_tests/lastBuild/api/json')
 
+LAST_BUILD_TAR_BASE = ('https://fuel-jenkins.mirantis.com/job/'
+                       'nailgun_performance_tests/lastBuild/artifact/'
+                       'results/results/')
+
 CSV_URL = ('https://fuel-jenkins.mirantis.com/job/nailgun_performance_tests'
-           '/lastSuccessfulBuild/artifact/nailgun'
+           '/lastBuild/artifact/nailgun'
            '/nailgun_perf_test_report.csv')
 
 CSV_TARGET_PATH = '/usr/share/nginx/html/test_report.csv'
+
+DOT_TARGET_DIR = 'dot/'
 
 
 try:
@@ -20,9 +27,24 @@ try:
 except (IOError, ValueError):
     previous_build_number = 0
 
-current_build_number = json.loads(urllib.urlopen(BUILD_URL).read())['number']
+current_build_info = json.loads(urllib.urlopen(LAST_BUILD_URL).read())
 
-if current_build_number > previous_build_number:
-    urllib.urlretrieve(CSV_URL, CSV_TARGET_PATH)
+current_build_number = current_build_info['number']
+
+if current_build_number >= previous_build_number:
     with open('build_number', 'w') as bn_file:
         bn_file.write(str(current_build_number))
+
+    #urllib.urlretrieve(CSV_URL, CSV_TARGET_PATH)
+
+    arts = [x for x in current_build_info['artifacts'] if 'tar.gz' in x['fileName']]
+
+    for artifact in arts:
+        filename = artifact['fileName']
+        print filename
+        urllib.urlretrieve(
+            LAST_BUILD_TAR_BASE + filename,
+            DOT_TARGET_DIR + filename 
+        )
+
+        subprocess.call(["tar", "-zxvf", DOT_TARGET_DIR + filename, '-C', DOT_TARGET_DIR])
