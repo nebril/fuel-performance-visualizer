8faa5c5456822f19fdb722142e268ca31a25e23e
Maciej Kwiek <mkwiek@mirantis.com>
2015-02-13 09:25:00 +0100

    Pushd and popd in deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,18 @@ set -ex
 . $NVM_DIR/nvm.sh
 nvm use 0.10
 git pull
-cd web
+pushd web
 npm install
 bower install
 grunt build
-rm -rf /usr/share/nginx/html/*
+
+pushd /usr/share/nginx/html
+rm -rf 404.html  bower_components  favicon.ico  images  index.html  robots.txt  scripts  styles  views
+
+popd
 cp -r dist/* /usr/share/nginx/html
-cd ..
+
+popd
 python generator/generate_data.py
 
 mv dot /usr/share/nginx/html/dot
