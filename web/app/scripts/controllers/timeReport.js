ece7ba97bae5855ebb1a6c37663a74628e4766df
Maciej Kwiek <mkwiek@mirantis.com>
2015-03-13 12:50:57 +0100

    Selected tests in url and whitespace retab

--- web/app/scripts/controllers/timeReport.js
+++ web/app/scripts/controllers/timeReport.js
@@ -2,83 +2,97 @@
 
 angular.module('fuelPerformanceVisualizerApp')
 .controller('TimeReportCtrl', function ($scope, $rootScope, $window, $routeParams, $location, testData) {
-	var smallChartXLabels = 6;
-	var bigChartXLabels = 15;
-	$scope.config = {
-		tooltips: true,
-		labels: false,
-		legend: {
-			display: true,
-			position: 'left',
-		},
-		lineLegend: 'traditional',
-		colors: [],
-		xAxisMaxTicks: smallChartXLabels,
-	};
+    var smallChartXLabels = 6;
+    var bigChartXLabels = 15;
+    $scope.config = {
+        tooltips: true,
+        labels: false,
+        legend: {
+            display: true,
+            position: 'left',
+        },
+        lineLegend: 'traditional',
+        colors: [],
+        xAxisMaxTicks: smallChartXLabels,
+    };
 
-	$scope.chartType = 'line';
+    $scope.chartType = 'line';
 
-	$scope.tests = testData;
+    $scope.tests = testData;
 
-	if(typeof $rootScope.datapoints === 'undefined')
-		$rootScope.datapoints = 5;
+    if(typeof $rootScope.datapoints === 'undefined')
+        $rootScope.datapoints = 5;
 
-	if('lastDataPoints' in $routeParams)
-		$rootScope.datapoints = parseInt($routeParams.lastDataPoints);
+    if('lastDataPoints' in $routeParams)
+        $rootScope.datapoints = parseInt($routeParams.lastDataPoints);
 
-	if(typeof $rootScope.search === 'undefined')
-		$rootScope.search = {
-			    name: ''
-		};
+    if(typeof $rootScope.search === 'undefined')
+        $rootScope.search = {
+                name: ''
+        };
 
-	var reloadCharts = function() {
-		_.forEach($scope.tests, function(test){
-			var slicer = - $scope.datapoints;
-			test.data = test.originalData.slice(slicer);
-			test.config = $scope.getConfig(test);
-		});
+    var reloadCharts = function() {
+        _.forEach($scope.tests, function(test){
+            var slicer = - $scope.datapoints;
+            test.data = test.originalData.slice(slicer);
+            test.config = $scope.getConfig(test);
+        });
 
-		if($scope.bigTest) {
-			$scope.bigTest.config.xAxisMaxTicks = bigChartXLabels;
-		}
-		$rootScope.datapoints = $scope.datapoints;
-		$location.search('lastDataPoints', $scope.datapoints);
+        if($scope.bigTest) {
+            $scope.bigTest.config.xAxisMaxTicks = bigChartXLabels;
+        }
+        $rootScope.datapoints = $scope.datapoints;
+        $location.search('lastDataPoints', $scope.datapoints);
 
-	};
+    };
 
-	$scope.bigTest = null;
-	$scope.selectBigTest = function(t) {
-		if($scope.bigTest) {
-			$scope.bigTest.config.xAxisMaxTicks = smallChartXLabels;
-		}
-		$scope.showBigTest = true;
-		$scope.bigTest = t;
-		$scope.bigTest.config.xAxisMaxTicks = bigChartXLabels;
-		$window.scrollTo(0,0);
-	};
-	$scope.deselectBigTest = function() {
-		$scope.showBigTest = false;
-		$scope.bigTest.config.xAxisMaxTicks = smallChartXLabels;
-		$scope.bigTest = null;
-	};
-	$scope.showBigTest = false;
+    $scope.bigTest = null;
+    $scope.selectBigTest = function(t) {
+        if($scope.bigTest) {
+            $scope.bigTest.config.xAxisMaxTicks = smallChartXLabels;
+        }
+        $scope.showBigTest = true;
+        $scope.bigTest = t;
+        $scope.bigTest.config.xAxisMaxTicks = bigChartXLabels;
+        $window.scrollTo(0,0);
+        $location.search('selectedTest', t.name);
+    };
+    $scope.deselectBigTest = function() {
+        $scope.showBigTest = false;
+        $scope.bigTest.config.xAxisMaxTicks = smallChartXLabels;
+        $scope.bigTest = null;
+        $location.search('selectedTest', '');
+    };
+    $scope.showBigTest = false;
 
-	$scope.$watch('datapoints', reloadCharts);
+    $scope.$watch('datapoints', reloadCharts);
 
-	var stringToColour = function(str) {
-		for (var i = 0, hash = 0; i < str.length; hash = str.charCodeAt(i++) + ((hash << 5) - hash));
-		for (var i = 0, colour = "#"; i < 3; colour += ("00" + ((hash >> i++ * 8) & 0xFF).toString(16)).slice(-2));
-		return colour;
-	};	
+    var stringToColour = function(str) {
+        for (var i = 0, hash = 0; i < str.length; hash = str.charCodeAt(i++) + ((hash << 5) - hash));
+        for (var i = 0, colour = "#"; i < 3; colour += ("00" + ((hash >> i++ * 8) & 0xFF).toString(16)).slice(-2));
+        return colour;
+    };
 
-	$scope.getConfig = function(test) {
-		var config = $scope.config;
-		if(test) {
-			config = (angular.extend({}, $scope.config, {colors: [stringToColour(test.name)]}));
-		} else {
-			config = $scope.config;
-		}
-		return config;
+    $scope.getConfig = function(test) {
+        var config = $scope.config;
+        if(test) {
+            config = (angular.extend({}, $scope.config, {colors: [stringToColour(test.name)]}));
+        } else {
+            config = $scope.config;
+        }
+        return config;
 
-	};
+    };
+
+    if('selectedTest' in $routeParams) {
+        var provided = _.filter($scope.tests, function(t){
+            return t.name===$routeParams.selectedTest;
+        })[0];
+        console.log(provided);
+        if(provided) {
+            reloadCharts()
+            $scope.selectBigTest(provided);
+        }
+    }
+
 });
